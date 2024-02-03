7bbd89a597932c6903f6c4c0ab118de5a8bbbc9f
6410110118 <6410110118@psu.ac.th>
2024-02-03 17:53:35 +0700

    finish

---
+++ Assets/Scripts/DeathBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBehavior : StateMachineBehaviour {
+
+    private Boss boss;
+	private Player player;
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        boss = animator.GetComponent<Boss>();
+        boss.isDead = true;
+		player = animator.GetComponent<Player>();
+        if (player != null)
+        {
+            player.isDead = false; // กำหนดค่า isDead ให้ถูกต้อง
+        }
+
+    }
+
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+       
+    }
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+	}
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public int health;
+    public float speed;
+    public int damage;
+    private float timeBtwDamage = 1.5f;
+
+    public Animator camAnim;
+    private Animator anim;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public Slider healthBar;
+    public bool isDead;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+        moveVelocity = moveInput.normalized * speed;
+
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else {
+            anim.SetBool("isRunning", false);
+        }
+
+        
+
+        if (health <= 10)
+        {
+            anim.SetTrigger("dead");
+        }
+
+        // give the player some time to recover before taking more damage !
+        if (timeBtwDamage > 0)
+        {
+            timeBtwDamage -= Time.deltaTime;
+        }
+
+        healthBar.value = health;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+    
+
+}
